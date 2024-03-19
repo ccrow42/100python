07a49d4cf446049e8bafcface90aad7e5bb800b0
Chris Crow <chris@ccrow.org>
2024-03-18 18:53:38 -0700

    day 7 done

--- projects/06 - Functions and Karel/learning.py
+++ projects/06 - Functions and Karel/learning.py
@@ -1,10 +1,9 @@
-print ("Hello")
-numChar = len("Hello")
-print(numChar)
-
 def myFunction():
     print("Hello")
     print("Bye")
 
 myFunction()
+
+
+# this is cool, especially for kids: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
 

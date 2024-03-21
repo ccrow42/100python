d14317b7999c6ced65940b43599d0ca18b8df773
ccrow42 <chris@ccrow.org>
2024-03-21 15:39:14 -0700

    day 10 done

---
+++ projects/10 - func with outputs/learning.py
@@ -0,0 +1,17 @@
+def my_function():
+    result = 3 * 2
+    return result
+
+output = my_function()
+
+print (output)
+
+
+###
+
+
+
+def format_name(f_name, l_name):
+    return (f_name.title() + " " + l_name.title())
+
+print(format_name("cHris", "crow"))

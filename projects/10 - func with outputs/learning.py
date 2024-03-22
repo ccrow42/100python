d1aef50bc4fb1c825ef8fd604d352cc9db0141fa
ccrow42 <chris@ccrow.org>
2024-03-22 09:19:04 -0700

    day 11 done

--- projects/10 - func with outputs/learning.py
+++ projects/10 - func with outputs/learning.py
@@ -12,6 +12,17 @@ print (output)
 
 
 def format_name(f_name, l_name):
+    """
+    Take a first and last name adn format it to return a titlecase version of the name.
+    The return is a single string that has been concatinated
+    """
+    if f_name == "" or l_name == "":
+        # return
+        return "invalid inputs"
     return (f_name.title() + " " + l_name.title())
 
 print(format_name("cHris", "crow"))
+
+ 
+# docstrings
+

4f30e8d14aea61d7e86cca251f65438279474f04
Chris Crow <chris@ccrow.org>
2024-03-26 10:40:24 -0700

    day 13 done

--- projects/13 - Debugging/main.py
+++ projects/13 - Debugging/main.py
@@ -22,24 +22,26 @@
 #   print("You are a Gen Z.")
 
 # # Fix the Errors
-age = int(input("How old are you?"))
-if age > 18:
-    print(f"You can drive at age {age}.")
+# age = int(input("How old are you?"))
+# if age > 18:
+#     print(f"You can drive at age {age}.")
 
 # #Print is Your Friend
 # pages = 0
 # word_per_page = 0
 # pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
+# # the issue was using the == instead of =
+# word_per_page = int(input("Number of words per page: "))
+# print (f"{pages} {word_per_page}")
 # total_words = pages * word_per_page
 # print(total_words)
 
 # #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list)
 
-# mutate([1,2,3,5,8,13])
+mutate([1,2,3,5,8,13])
